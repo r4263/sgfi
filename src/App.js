9d52559d9895bcdc7862fa10458b45a0f5c14ca5
maycon <maycon.jung7@gmail.com>
2023-04-16 00:40:27 -0300

    lapidando login form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
 import { Button } from 'reactstrap';
 import { Form } from 'reactstrap';
 import { FormGroup } from 'reactstrap';
@@ -10,43 +11,44 @@ import { Label } from 'reactstrap';
 import { Input } from 'reactstrap';
 import { FormText } from 'reactstrap';
 
-import {logo} from './content/svg/pharmacy.svg'
+import { logo } from './content/svg/pharmacy.svg'
 
 
 
 function App() {
+
+  // const [message, setMessage] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
-
       <div className='logindivform'>
-        
         <Form>
-        <div>
-        <a>
-          Sistema de Gerenciamento de Fármacos e Insumos 
-        </a>
-        </div>
+          <div>
+            <a>
+              Sistema de Gerenciamento de Fármacos e Insumos
+            </a>
+          </div>
           <FormGroup>
-            <Label for="exampleEmail">
+            <Label for="login">
               Usuário:
             </Label>
             <Input
-              id="exampleEmail"
-              name="email"
+              id="login"
+              name="login"
               placeholder="Login"
               type="email"
             />
           </FormGroup>
           <FormGroup>
-            <Label for="examplePassword">
+            <Label for="pwd">
               Senha:
             </Label>
             <Input
-              id="examplePassword"
-              name="password"
+              id="pwd"
+              name="pwd"
               placeholder="○○○○○○○"
               type="password"
             />
@@ -58,12 +60,15 @@ function App() {
               Lembrar credenciais
             </Label>
           </FormGroup>
-          <Button>
-            Entrar
-          </Button>
+          <input
+            id='loginbtn'
+            name='loginbtn'
+            type='submit'
+            value='Entrar'
+            disabled
+          />
         </Form>
       </div>
-
     </div>
   );
 }
