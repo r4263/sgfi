81a88e37b09784d762dad14baa85b91a85873a27
maycon <maycon.jung7@gmail.com>
2023-04-16 01:19:03 -0300

    configuração de rotas e pagina principal

--- src/App.js
+++ src/App.js
@@ -1,76 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from "react-dom";
+import LoginForm from "./content/pages/LoginForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//import logo from './logo.svg';
+//stylesheets
 import './App.css';
-
-import { useState } from 'react';
-import { Button } from 'reactstrap';
-import { Form } from 'reactstrap';
-import { FormGroup } from 'reactstrap';
-import { Label } from 'reactstrap';
-import { Input } from 'reactstrap';
-import { FormText } from 'reactstrap';
-
-import { logo } from './content/svg/pharmacy.svg'
-
 
 
 function App() {
-
-  // const [message, setMessage] = useState('');
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
-      <div className='logindivform'>
-        <Form>
-          <div>
-            <a>
-              Sistema de Gerenciamento de Fármacos e Insumos
-            </a>
-          </div>
-          <FormGroup>
-            <Label for="login">
-              Usuário:
-            </Label>
-            <Input
-              id="login"
-              name="login"
-              placeholder="Login"
-              type="email"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="pwd">
-              Senha:
-            </Label>
-            <Input
-              id="pwd"
-              name="pwd"
-              placeholder="○○○○○○○"
-              type="password"
-            />
-          </FormGroup>
-          <FormGroup check>
-            <Input type="checkbox" />
-            {' '}
-            <Label check>
-              Lembrar credenciais
-            </Label>
-          </FormGroup>
-          <input
-            id='loginbtn'
-            name='loginbtn'
-            type='submit'
-            value='Entrar'
-            disabled
-          />
-        </Form>
-      </div>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route index element={<LoginForm />} />
+        <Route path='/home' element={<teste />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
---
+++ src/content/pages/LoginForm.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../stylesheets/sgfi.css'
+
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { useState } from 'react';
+import { Button } from 'reactstrap';
+import { Form } from 'reactstrap';
+import { FormGroup } from 'reactstrap';
+import { Label } from 'reactstrap';
+import { Input } from 'reactstrap';
+import { FormText } from 'reactstrap';
+
+
+function LoginForm() {
+
+    // const [message, setMessage] = useState('');
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        </header>
+        <div className='logindivform'>
+          <Form>
+            <div>
+              <a>
+                Sistema de Gerenciamento de Fármacos e Insumos
+              </a>
+            </div>
+            <FormGroup>
+              <Label for="login">
+                Usuário:
+              </Label>
+              <Input
+                id="login"
+                name="login"
+                placeholder="Login"
+                type="email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="pwd">
+                Senha:
+              </Label>
+              <Input
+                id="pwd"
+                name="pwd"
+                placeholder="○○○○○○○"
+                type="password"
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Input type="checkbox" />
+              {' '}
+              <Label check>
+                Lembrar credenciais
+              </Label>
+            </FormGroup>
+            <input
+              id='loginbtn'
+              name='loginbtn'
+              type='submit'
+              value='Entrar'
+              disabled
+            />
+          </Form>
+        </div>
+      </div>
+    );
+  }
+  
+  export default LoginForm;
