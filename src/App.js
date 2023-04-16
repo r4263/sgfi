e09bf1ae1d4ec24a32edf11c4421160a36b4d273
maycon <maycon.jung7@gmail.com>
2023-04-15 21:38:35 -0300

    modelando login form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //import logo from './logo.svg';
 import './App.css';
-import './loginform.js'
+
+import { Button } from 'reactstrap';
+import { Form } from 'reactstrap';
+import { FormGroup } from 'reactstrap';
+import { Label } from 'reactstrap';
+import { Input } from 'reactstrap';
+import { FormText } from 'reactstrap';
+
+import {logo} from './content/svg/pharmacy.svg'
 
 
 
@@ -11,8 +19,51 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>     
-      <renderloginform/>
+      </header>
+
+      <div className='logindivform'>
+        
+        <Form>
+        <div>
+        <a>
+          Sistema de Gerenciamento de Fármacos e Insumos 
+        </a>
+        </div>
+          <FormGroup>
+            <Label for="exampleEmail">
+              Usuário:
+            </Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="Login"
+              type="email"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">
+              Senha:
+            </Label>
+            <Input
+              id="examplePassword"
+              name="password"
+              placeholder="○○○○○○○"
+              type="password"
+            />
+          </FormGroup>
+          <FormGroup check>
+            <Input type="checkbox" />
+            {' '}
+            <Label check>
+              Lembrar credenciais
+            </Label>
+          </FormGroup>
+          <Button>
+            Entrar
+          </Button>
+        </Form>
+      </div>
+
     </div>
   );
 }
